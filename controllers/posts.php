77d8fc4afc10ca93ea883d2f964caad84d7040c8
Jaanika Lehtsalu <jaanika.lehtsalu@khk.ee>
2014-02-10 00:05:06 +0200

    Add single post viewing functionality

--- controllers/posts.php
+++ controllers/posts.php
@@ -2,6 +2,11 @@
 
 class posts extends Controller{
 
+    function view(){
+        $post_id=$this->params[0];
+        $this->post=get_first("SELECT * FROM post NATURAL JOIN user where post_id='$post_id'");
+    }
+
 	function index(){
 		$this->users = get_all("SELECT * FROM user");
         $this->posts =get_all("SELECT * FROM post");
