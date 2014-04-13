07fe17abefc4ab2ddb5ee5050978a6101da85ab2
Jaanika Lehtsalu <jaanika.lehtsalu@khk.ee>
2014-04-13 17:11:56 +0300

    Fix tags

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,16 +1,28 @@
-<? foreach ($posts as $post): ?>
+
+<!-- Code for ajax -->
+<script type="text/javascript">
+    function clickme() {
+        $.post("<?=BASE_URL?>welcome", $( "#ajax-form" ).serialize(), function (data) {
+            $(".result").html(data);
+        });
+    }
+</script>
+
+
+
+<?foreach($posts as $post):?>
     <div class="span8">
-    <h1><a href="<?= BASE_URL ?>posts/view/<?= $post['post_id'] ?>"><?= $post['post_subject'] ?></a></h1>
-
-    <p><?= $post['post_text'] ?></p>
-    <div>
-    <span class="badge badge-success">Posted <?=$post["post_created"]?></span>
-    <span class="badge badge-success">By <?=$post["username"]?></span>
-    <div class="pull-right">
-
-        <? if (!empty($tags[$post['post_id']])) foreach ($tags[$post['post_id']] as $tag): ?>
-            <a href="<?= BASE_URL ?>tags/view/<?= $tag['tag_id'] ?>"><span class="label"
-         style="background-color: #60de35"><?= $tag['tag_name'] ?></span></a>
-        <? endforeach ?>
+        <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post["post_subject"]?></a></h1>
+        <p><?=$post["post_text"]?></p>
+        <div>
+            <span class="badge badge-success">Posted <?=$post["post_created"]?></span>
+            <span class="badge badge-success">By <?=$post["username"]?></span>
+            <div class="pull-right">
+                <?foreach ($tags   [$post	['post_id']] as $tag):?>
+                    <a href="<?=BASE_URL?>tags/view/<?=$tag?>"><span class = "label" style="background-color: #93de7e"><?=$tag?></span></a>
+                <?endforeach?>
+            </div>
+        </div>
+        <hr>
     </div>
-    <hr><? endforeach ?>
+<?endforeach?>
