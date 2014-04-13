1c901db592b1277ed6cff9b9b32be62bb1e36e91
Jaanika Lehtsalu <jaanika.lehtsalu@khk.ee>
2014-04-13 17:26:31 +0300

    Fix tags

--- controllers/posts.php
+++ controllers/posts.php
@@ -1,44 +1,29 @@
 <?php
 
-class posts extends Controller
-{
-    function view()
-    {
-        if (!empty($_POST))
-            $this->save();
-        $post_id = $this->params[0];
-        $this->post = get_first("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
-        $this->post = get_one("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
-        $this->tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
+class posts extends Controller{
 
-
-
-    }
-
-    function index()
-    {
-        $this->posts = get_all("SELECT * FROM post NATURAL JOIN user ORDER BY post_created DESC");
-        $this->users = get_all("SELECT * FROM user");
-
-        // prepare tags array
-        $_tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag");
-        foreach ($_tags as $tag) {
-            $this->tags[$tag['post_id']][] = $tag['tag_name'];
+    function index(){
+        $this->posts = get_all("SELECT * FROM post NATURAL JOIN user");
+        $_tags=get_all("SELECT * FROM post_tags NATURAL JOIN tag");
+        $this->tags = array();
+        foreach ($_tags as $tag){
+            $this->tags[$tag['post_id']][]= array('tag_id'=>$tag['tag_id'], 'tag_name'=>$tag['tag_name']);
         }
+    }
 
+    function index_ajax(){
+        echo "\$_POST:<br>";
+    }
 
+    function index_post(){
+        echo "\$_POST:<br>";
+    }
+
+    function view() {
+        $post_id = $this->params[0];
+        $this->comments = get_all("SELECT * FROM post_comments NATURAL JOIN comment WHERE post_id='$post_id'");
+        $this->tags=get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
+        $this->post = get_first("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
 
     }
-}
-
-function index_ajax()
-{
-    echo "\$_POST:<br>";
-    var_dump($_POST);
-}
-
-function index_post()
-{
-    echo "\$_POST:<br>";
-    var_dump($_POST);
 }
